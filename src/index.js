19cf8eb89a5a594f0a9a569f70ab35cd409891ad
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2023-01-20 22:03:00 -0300

    alterando nome do projeto no ghpages e introduzindo hashRouter no lugar de BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import store from './store';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={ store }>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root'),
 );
